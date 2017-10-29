7a837064cf644a1849112463477fc21c609adba4
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-10-29 18:09:54 -0400

    Updated compute_cost function

--- computeCostOfDifference.m
+++ computeCostOfDifference.m
@@ -3,5 +3,12 @@ function [cost] = computeCostOfDifference(croppedEdgeMap, libraryEdgeMap)
 % @return: libraryEdgeMap: a edge map in the library of labeled edge maps 
 % @return: cost: the cost between the croppedEdgeMap and croppedEdgeMap
 
+lib_points = (libraryEdgeMap==1);
+query_points = (croppedEdgeMap==1);
+[~,d] = dsearchn(lib_points, query_points);
+
+cost = sum(d);
+
+
 end
 
