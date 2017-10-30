8cb339cb8cab501ea4e29e3eb71e664bd11d6dd2
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-10-29 20:06:03 -0400

    Cost function updated to correct equation

--- computeCostOfDifference.m
+++ computeCostOfDifference.m
@@ -3,12 +3,15 @@ function [cost] = computeCostOfDifference(croppedEdgeMap, libraryEdgeMap)
 % @return: libraryEdgeMap: a edge map in the library of labeled edge maps 
 % @return: cost: the cost between the croppedEdgeMap and croppedEdgeMap
 
-lib_points = (libraryEdgeMap==1);
-query_points = (croppedEdgeMap==1);
+%assuming both input images will be the same size
+[n,m] = size(croppedEdgeMap);
+
+[x,y] = meshgrid(1:m,1:n);
+lib_points = [x(libraryEdgeMap==1),y(libraryEdgeMap==1)];
+query_points = [x(croppedEdgeMap==1),y(croppedEdgeMap==1)];
 [~,d] = dsearchn(lib_points, query_points);
 
 cost = sum(d);
 
 
 end
-
