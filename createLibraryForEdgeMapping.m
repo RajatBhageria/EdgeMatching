db45592843caf25e8997096e2df8584ee0e9b7b6
Rajat Bhageria <rajat1996@gmail.com>
2017-11-02 15:51:21 -0400

    canny fist

--- createLibraryForEdgeMapping.m
+++ createLibraryForEdgeMapping.m
@@ -5,14 +5,16 @@ function [libraryOfEdgeMaps] = createLibraryForEdgeMapping(images, scale)
 %% Items in libraryOfEdgeMaps and labelsOfLibrary correspond to each other 
 
 for image = 1:size(images,1)
+   
+    %% Do canny edge detection 
+    images{image,1} = cannyEdge(images{image,1}); 
+    
     %% crop all the images
     images{image,1} = cropEdgeMap(images{image,1});
     
     %% Scale all the images to 100x100
     images{image,1} = imresize(images{image,1},scale);
-    
-    %% Do cann
-    images{image,1} = cannyEdge(images{image,1}); 
+
 end 
 
 %% Return the libraryOfEdgeMaps
